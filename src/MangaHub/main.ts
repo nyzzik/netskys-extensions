789e71260e80da3b13431e433a62885008c70b1f
Josh Biedrzycki <nyzzik@gmail.com>
2024-11-16 13:56:52 -0500

    Fix bug

--- src/MangaHub/main.ts
+++ src/MangaHub/main.ts
@@ -2,7 +2,7 @@ import { Mangahub as _Mangahub } from "./Mangahub";
 
 import { CompatWrapper } from '@paperback/types/lib/compat/0.8'
 
-export const AsuraScans = CompatWrapper(
+export const Mangahub = CompatWrapper(
     {registerHomeSectionsInInitialise: true},
     new _Mangahub(undefined)
 )
--- src/ManhuaPlus/main.ts
+++ src/ManhuaPlus/main.ts
@@ -2,7 +2,7 @@ import { ManhuaPlus as _ManhuaPlus } from "./ManhuaPlus";
 
 import { CompatWrapper } from '@paperback/types/lib/compat/0.8'
 
-export const AsuraScans = CompatWrapper(
+export const ManhuaPlus = CompatWrapper(
     {registerHomeSectionsInInitialise: true},
     new _ManhuaPlus(undefined)
 )
